372dfeeb419a662a09c4a97a9be7ae0a5cbd7a60
Todd Bryant <hello@toddaas.com>
2024-04-02 10:18:50 +1100

    moved lambda from external python code read from FS to an inline string

--- src/autoscaling/warmpool.ts
+++ src/autoscaling/warmpool.ts
@@ -1,4 +1,3 @@
-import { readFileSync } from 'fs';
 import * as cdk from 'aws-cdk-lib';
 import * as as from 'aws-cdk-lib/aws-autoscaling';
 import * as cwe from 'aws-cdk-lib/aws-events';
@@ -61,7 +60,19 @@ export class WarmPool extends Construct {
         runtime: lambda.Runtime.PYTHON_3_9,
         handler: 'index.handler',
         code: new lambda.InlineCode(
-          readFileSync('./lib/LifeCycleActionLambda.py', 'utf8'),
+          `
+          import json
+          import boto3
+          
+          def handler(event, context):
+              ## This Lambda function will be triggered on all events received by EventBridge for instances moving from the Warmed:Running, Warmed:Stopped or Warmed:Hibernated state of an ASG's Warmpool into service in the ASG.  This is required, since by design when using autoscaling groups with Warm pools an instance will need to call complete_lifecycle_action twice.  Once when the instance is launched Warm:Pending:Wait the initially bootstrapped and then a second time when there is Scale out so an Instance moves from a Warm pool Pending:Wait into the autoscaling group to be put InService. https://docs.aws.amazon.com/images/autoscaling/ec2/userguide/images/warm-pools-scale-out-event-diagram.png  The function of this Lambda is for the second complete_lifecycle_action API call to put Warm Pool instances automatically into InService.
+              client = boto3.client('autoscaling')
+              response = client.complete_lifecycle_action(LifecycleHookName=event['detail']['LifecycleHookName'],
+              LifecycleActionToken=event['detail']['LifecycleActionToken'],
+              AutoScalingGroupName=event['detail']['AutoScalingGroupName'],
+              LifecycleActionResult='CONTINUE',
+              InstanceId=event['detail']['EC2InstanceId'])
+          `,
         ),
         role: asgCwEventRuleLambdaRole,
       },
