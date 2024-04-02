7e47befd90fdcb817bcb1ad5c33fe2b604aadbde
Todd Bryant <hello@toddaas.com>
2024-04-02 14:00:08 +1100

    Revert "break pipeline"
    This reverts commit f4c19210fd296ed2a9e701ce66d695afef93650a.

--- .projenrc.ts
+++ .projenrc.ts
@@ -25,7 +25,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   releaseToNpm: true,
   npmAccess: NpmAccess.PUBLIC,
   npmProvenance: true,
-  // Projen automatically updates dependancies as part of github actions - below auto approves them
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
